75c29ce617dcfc38625a4374dbb4473beceb3274
valou <valentin.depatureaux@epita.fr>
2022-01-25 15:41:58 +0100

    Persistence en base de données

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -2,16 +2,26 @@ package fr.lernejo.todo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoListController {
+
+    private TodoRepository repository;
+
+    public TodoListController(TodoRepository repo){
+        this.repository = repo;
+    }
+
     @PostMapping("api/todo")
-    public void post(Todo todo){
+    public void post(@RequestBody TodoEntity todo){
+        this.repository.save(todo);
     }
 
     @GetMapping("api/todo")
-    public Todo get(){
-        return new Todo("enculé","Théo");
+    public Iterable<TodoEntity> get()
+    {
+        return this.repository.findAll();
     }
 }
